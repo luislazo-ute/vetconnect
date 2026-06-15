6dc95824941a2f0f3339e531cb1f93d2f7c05bfe
Luis Lazo <luis.lazo@ute.edu.ec>
2026-06-14 19:53:17 -0500

    Fase E: StandardPagination con page_size configurable (max 100) global

--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'pacientes.pagination.StandardPagination',
     'PAGE_SIZE': 10,
 }
